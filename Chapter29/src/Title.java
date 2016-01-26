ae1fd00d0de530fe424bdbab013d24265e31652e
Bilal Reza <rezasb@s.dcsdk12.org>
2016-01-26 09:44:29 -0700

    Done with 3

--- Chapter29/src/Title.java
+++ Chapter29/src/Title.java
@@ -5,9 +5,17 @@ public class Title {
 	public static void main(String[] args) {
 		System.out.println("Enter a name: ");
 		Scanner scan = new Scanner(System.in);
-		String name = scan.next();
-		if ((name == "Amy") || (name == "Buffy") || (name == "Cathy")) {
+		String name = scan.nextLine();
+		if (name.indexOf("Amy") >= 0
+			|| name.indexOf("Buffy") >= 0
+			|| name.indexOf("Cathy") >= 0) {
 			System.out.println("Ms. " + name);
+		} else if (name.indexOf("Elroy") >= 0
+			|| name.indexOf("Fred") >= 0
+			|| name.indexOf("Graham") >=0) {
+			System.out.println("Mr. " + name);
+		} else {
+			System.out.println(name);
 		}
 	}
 
